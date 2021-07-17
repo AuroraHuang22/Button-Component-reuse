051d5f1b24d694236a430998c1a81d558f62e045
AuroraHuang22 <aurora90376@gmail.com>
2021-07-17 18:44:11 +0800

    Complete Demo page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,338 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Button from "./Components/Button";
+import { CodeBlock, purebasic } from "react-code-blocks";
+const Header = styled.header``;
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1280px;
+  margin: 0 auto;
+  section {
+    border: 1px solid #f1f1f1;
+    padding: 20px;
+    margin: 10px 0px;
+  }
+`;
+const Table = styled.div`
+  table,
+  th,
+  td {
+    border: 1px solid black;
+    padding: 6px 18px;
+  }
+  table {
+    border-collapse: collapse;
+  }
+`;
+const Description = styled.span`
+  line-height: 48px;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+`;
+const CodeExample = styled.div`
+  width: 70%;
+`;
+const Demo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 30%;
+`;
 
 function App() {
+  const btnExample = {
+    creation: `import Button from "./Components/Button";
+  function App(){
+       return 
+        <Button />
+      }`,
+    default: `<Button />`,
+    language: `<Button
+    theme="green"
+    icon="language"
+    text="Languange"
+    drop={true}
+  />`,
+    fullScreen: `<Button theme="cyan" icon="full-screen" text="Full Screen" />
+<Button theme="disabled" icon="full-screen" text="Full Screen" />`,
+    upload: `<Button
+    theme="white"
+    icon="upload"
+    iconPosition="right"
+    text="Upload"
+  />
+  <Button
+    theme="outline"
+    icon="next-grey"
+    iconPosition="right"
+    text="Next"
+  />`,
+    icon: ` <Button circle={true} />`,
+    confirm: `<Button
+    theme="green"
+    icon="placeholder"
+    iconPosition="right"
+    text="confirm"
+    width="min"
+  />
+  <Button
+    theme="green"
+    icon="placeholder"
+    iconPosition="right"
+    text="confirm"
+    width="default"
+  />
+  <Button
+    theme="green"
+    icon="placeholder"
+    iconPosition="right"
+    text="confirm"
+    width="max"
+  />`,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <AppContainer>
+        <h1>Button Component</h1>
+        <div>
+          <h2>Props</h2>
+          <hr />
+          <Table>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Option</th>
+                  <th>Type</th>
+                  <th>Default</th>
+                  <th>Description</th>
+                </tr>
+                <tr>
+                  <td>theme</td>
+                  <td>string</td>
+                  <td>white</td>
+                  <td>One of green, cyan, white, outline, disabled.</td>
+                </tr>
+                <tr>
+                  <td>icon</td>
+                  <td>string</td>
+                  <td>placeholder</td>
+                  <td>
+                    File name of icon. feel free to replace default icon by
+                    using 'full-screen', 'upload', 'language', 'next-grey',
+                    'next-cyan'.
+                  </td>
+                </tr>
+                <tr>
+                  <td>iconPosition</td>
+                  <td>string</td>
+                  <td>left</td>
+                  <td>
+                    One of left or right, if the drop is set to true, the
+                    iconPosition will be set left automatic.
+                  </td>
+                </tr>
+                <tr>
+                  <td>text</td>
+                  <td>string</td>
+                  <td>-</td>
+                  <td>
+                    Button's content. If you want to use the icon button, set
+                    the text to the empty string.
+                  </td>
+                </tr>
+                <tr>
+                  <td>width</td>
+                  <td>string</td>
+                  <td>default</td>
+                  <td>Three type of width: min, default, max.</td>
+                </tr>
+                <tr>
+                  <td>drop</td>
+                  <td>boolean</td>
+                  <td>false</td>
+                  <td>Show the drop icon or not.</td>
+                </tr>
+                <tr>
+                  <td>circle</td>
+                  <td>boolean</td>
+                  <td>false</td>
+                  <td>Change the button to a circle style.</td>
+                </tr>
+              </tbody>
+            </table>
+          </Table>
+        </div>
+        <div>
+          <h2>Creation</h2>
+          <hr />
+          <Description>{`Simply create a button by using <Button/> component.`}</Description>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.creation}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button />
+            </Demo>
+          </Container>
+        </div>
+        <div>
+          <h2>Demo</h2>
+          <hr />
+        </div>
+        <section>
+          <h4>Default</h4>
+          <span>{`By default, all buttons will inherit <Button> props.`}</span>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.default}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button />
+            </Demo>
+          </Container>
+        </section>
+        <section>
+          <h4>Icon button</h4>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.icon}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button circle={true} />
+            </Demo>
+          </Container>
+        </section>
+        <section>
+          <h4>Full Screen button</h4>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.fullScreen}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button theme="cyan" icon="full-screen" text="Full Screen" />
+              <Button theme="disabled" icon="full-screen" text="Full Screen" />
+            </Demo>
+          </Container>
+        </section>
+        <section>
+          <h4>Upload, Next button</h4>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.upload}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button
+                theme="white"
+                icon="upload"
+                iconPosition="right"
+                text="Upload"
+              />
+              <Button
+                theme="outline"
+                icon="next-grey"
+                iconPosition="right"
+                text="Next"
+              />
+            </Demo>
+          </Container>
+        </section>
+        <section>
+          <h4>Languange button</h4>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.language}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+              />
+            </CodeExample>
+            <Demo>
+              <Button
+                theme="green"
+                icon="language"
+                text="Languange"
+                drop={true}
+              />
+            </Demo>
+          </Container>
+        </section>
+        <section>
+          <h4>Confirm button</h4>
+          <Container>
+            <CodeExample>
+              <CodeBlock
+                language="jsx"
+                text={btnExample.confirm}
+                showLineNumbers={true}
+                theme={purebasic}
+                wrapLines={true}
+                highlight="6,13,20"
+              />
+            </CodeExample>
+            <Demo>
+              <Button
+                theme="green"
+                icon="next-cyan"
+                iconPosition="right"
+                text="confirm"
+                width="min"
+              />
+              <Button
+                theme="green"
+                icon="next-cyan"
+                iconPosition="right"
+                text="confirm"
+                width="default"
+              />
+              <Button
+                theme="green"
+                icon="next-cyan"
+                iconPosition="right"
+                text="confirm"
+                width="max"
+              />
+            </Demo>
+          </Container>
+        </section>
+      </AppContainer>
+    </>
   );
 }
 
---
+++ src/Components/Button.js
@@ -0,0 +1,273 @@
+import styled from "styled-components";
+
+const Btn = styled.button`
+  position: relative;
+  box-sizing: border-box;
+  padding: ${(props) => {
+    if (props.width === "max" && props.drop) {
+      return "10px 16px 10px 128.5px";
+    } else if (props.text !== "" && props.width === "min") {
+      return "6px 16px";
+    } else if (props.text !== "" && props.width === "default") {
+      return "10px 16px";
+    } else if (props.text !== "" && props.width === "max") {
+      return "10px 128.5px";
+    } else if (props.text === "" && !props.circle) {
+      return "8px";
+    } else if (props.text === "" && props.circle) {
+      return "16px";
+    }
+  }};
+  margin: 5px auto;
+  display: flex;
+  flex-direction: ${(props) => {
+    if (props.drop === true) {
+      return "row";
+    } else if (props.iconPosition === "right") {
+      return "row-reverse";
+    } else if (props.iconPosition === "left") {
+      return "row";
+    }
+  }};
+  align-items: center;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${(props) => {
+    if (props.theme === "cyan") {
+      return "#28646e";
+    } else if (props.theme === "green" || props.theme === "confirm") {
+      return "#AAF1E7";
+    } else if (props.theme === "disabled") {
+      return "#9B9B9B";
+    }
+  }};
+  outline: none;
+  border: 2px solid;
+  border-color: ${(props) => {
+    if (props.theme === "cyan") {
+      return "#AAF1E7";
+    } else if (props.theme === "green" || props.theme === "confirm") {
+      return "#28646E";
+    } else if (props.theme === "plain" || props.theme === "white") {
+      return "#EBEBEB";
+    } else if (props.theme === "outline") {
+      return "#9b9b9b";
+    } else if (props.theme === "disabled") {
+      return "#EBEBEB";
+    }
+  }};
+  border-radius: ${(props) => {
+    if (props.circle) {
+      return "50%";
+    } else {
+      return "10px";
+    }
+  }};
+  background-color: ${(props) => {
+    if (props.theme === "cyan") {
+      return "#AAF1E7";
+    } else if (props.theme === "green" || props.theme === "confirm") {
+      return "#28646E";
+    } else if (
+      props.theme === "plain" ||
+      props.theme === "white" ||
+      props.theme === "outline"
+    ) {
+      return "#e9e9e9";
+    } else if (props.theme === "disabled") {
+      return "#EBEBEB";
+    }
+  }};
+  transition: all 0.2s;
+  cursor: ${(props) => {
+    if (props.theme === "disabled") {
+      return "not-allowed";
+    } else {
+      return "pointer";
+    }
+  }};
+
+  :hover {
+    border-color: ${(props) => {
+      if (props.theme === "cyan") {
+        return "#c4f5ee";
+      } else if (props.theme === "green" || props.theme === "confirm") {
+        return "#69939a";
+      } else if (
+        props.theme === "plain" ||
+        props.theme === "white" ||
+        props.theme === "outline"
+      ) {
+        return "#f1f1f1";
+      }
+    }};
+    background-color: ${(props) => {
+      if (props.theme === "cyan") {
+        return "#c4f5ee";
+      } else if (props.theme === "green" || props.theme === "confirm") {
+        return "#69939a";
+      } else if (
+        props.theme === "plain" ||
+        props.theme === "white" ||
+        props.theme === "outline"
+      ) {
+        return "#f1f1f1";
+      }
+    }};
+  }
+
+  :active {
+    border-color: ${(props) => {
+      if (props.theme === "cyan") {
+        return "#90d5cf";
+      } else if (props.theme === "green" || props.theme === "confirm") {
+        return "#205058";
+      } else if (
+        props.theme === "plain" ||
+        props.theme === "white" ||
+        props.theme === "outline"
+      ) {
+        return "#cbcbcb";
+      }
+    }};
+    background-color: ${(props) => {
+      if (props.theme === "cyan") {
+        return "#90d5cf";
+      } else if (props.theme === "green" || props.theme === "confirm") {
+        return "#205058";
+      } else if (
+        props.theme === "plain" ||
+        props.theme === "white" ||
+        props.theme === "outline"
+      ) {
+        return "#cbcbcb";
+      }
+    }};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+    position: ${(props) => {
+      if (props.width === "max") {
+        return "absolute";
+      } else {
+        return "static";
+      }
+    }};
+    top: ${(props) => {
+      if (props.width === "max") {
+        return "50%";
+      } else {
+        return "auto";
+      }
+    }};
+    right: ${(props) => {
+      if (props.width === "max" && props.iconPosition === "right") {
+        return "26px";
+      } else {
+        return "auto";
+      }
+    }};
+    left: ${(props) => {
+      if (props.width === "max" && props.iconPosition === "left") {
+        return "26px";
+      } else {
+        return "auto";
+      }
+    }};
+    transform: ${(props) => {
+      if (props.width === "max" && props.iconPosition === "right") {
+        return "translate(50%, -50%)";
+      } else if (props.width === "max" && props.iconPosition === "left") {
+        return "translate(-50%, -50%)";
+      } else {
+        return "none";
+      }
+    }};
+
+    display: ${(props) => {
+      if (props.width === "min") {
+        return "none";
+      } else {
+        return "block";
+      }
+    }};
+    justify-self: flex-end;
+    filter: ${(props) => {
+      if (props.theme === "disabled") {
+        return "grayscale(80%)";
+      }
+    }};
+    opacity: ${(props) => {
+      if (props.theme === "disabled") {
+        return "0.5";
+      }
+    }};
+    margin: ${(props) => {
+      if (props.text === "") {
+        return "0px";
+      } else if (props.iconPosition === "right") {
+        return "0px 0px 0px 14px";
+      } else {
+        return "0px 14px 0px 0px";
+      }
+    }};
+  }
+`;
+
+const Icon = styled.div`
+  display: ${(props) => {
+    if (props.width === "min") {
+      return "none";
+    } else {
+      return "block";
+    }
+  }};
+
+  background-image: url("./icons/drop.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  width: 20px;
+  height: 20px;
+  margin-left: ${(props) => {
+    if (props.width === "max") {
+      return "92.5px";
+    } else if (props.width === "default") {
+      return "12px";
+    }
+  }};
+`;
+
+function Button(props) {
+  const theme = props.theme ? props.theme : "white";
+  const iconPosition = props.iconPosition
+    ? props.drop
+      ? "left"
+      : props.iconPosition
+    : "left";
+  const text = props.text ? (props.circle ? "" : props.text) : "";
+  const icon = props.icon ? props.icon : "placeholder";
+  const width = props.width ? props.width : "default";
+  const circle = props.circle ? props.circle : false;
+  const drop = props.drop ? props.drop : false;
+  return (
+    <Btn
+      theme={theme}
+      width={width}
+      iconPosition={iconPosition}
+      circle={circle}
+      text={text}
+      drop={drop}
+    >
+      <img src={`./icons/${icon}.svg`} alt={`This is the icon of ${icon}`} />
+      {text}
+      {drop ? <Icon width={width} /> : null}
+    </Btn>
+  );
+}
+
+export default Button;
